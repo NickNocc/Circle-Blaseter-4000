acba3afb7cc72fe376894356308b59d525a97c98
NickNocc <haretakis4@gmail.com>
2022-04-12 12:57:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ const context = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+class Player {
+    constructor(x, y, radius, color) {
+      this.x = x;
+      this.y = y;
+      this.radius = radius;
+      this.color = color;
+    }
+  
+    draw() {
+      context.beginPath();
+      context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      context.fillStyle = this.color;
+      context.fill();
+    }
+  }
+  
+  const player = new Player(100, 100, 30, "blue");
+  player.draw();
+  
