2cc006c03e9230350a1eddf60230f08dd6c87320
NickNocc <haretakis4@gmail.com>
2022-04-12 12:58:48 -0700

    Changed spawn location

--- index.js
+++ index.js
@@ -5,21 +5,23 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 class Player {
-    constructor(x, y, radius, color) {
-      this.x = x;
-      this.y = y;
-      this.radius = radius;
-      this.color = color;
-    }
-  
-    draw() {
-      context.beginPath();
-      context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      context.fillStyle = this.color;
-      context.fill();
-    }
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
   }
-  
-  const player = new Player(100, 100, 30, "blue");
-  player.draw();
-  
+
+  draw() {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    context.fillStyle = this.color;
+    context.fill();
+  }
+}
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+const player = new Player(x, y, 30, "blue");
+player.draw();
