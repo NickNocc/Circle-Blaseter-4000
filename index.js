91de84c699e18a29680ec1b62730ed2c4825b726
NickNocc <haretakis4@gmail.com>
2022-04-12 13:00:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ class Projectile {
     this.color = color;
     this.velocity = velocity;
   }
+
+  draw() {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    context.fillStyle = this.color;
+    context.fill();
+  }
 }
 
 const x = canvas.width / 2;
